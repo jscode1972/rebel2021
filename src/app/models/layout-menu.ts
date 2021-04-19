471b56f77fd75f1b8fd861306037e5887704ff3c
jscode1972 <jscode1972@gmail.com>
2021-04-19 08:01:02 +0800

    path

--- src/app/models/layout-menu.ts
+++ src/app/models/layout-menu.ts
@@ -1,5 +1,6 @@
 class SubMenu {
   func: string;
+  route: string;
   allow: boolean;
   active: boolean;
 }
@@ -14,23 +15,23 @@ export const MENUS: LayoutMenu[] = [
   { main: '主要',
     state: '',
     funcs: [
-        {func: '主要功能1', allow: true, active: false },
-        {func: '主要功能2', allow: false, active: false },
-        {func: '主要功能3', allow: true, active: false }
+        {func: '主要功能1', route: '/aa', allow: true, active: false },
+        {func: '主要功能2', route: '/bb', allow: false, active: false },
+        {func: '主要功能3', route: '/cc', allow: true, active: false }
     ]
   },
   { main: '中等',
     state: '',
     funcs: [
-        {func: '次要功能1', allow: true, active: false },
-        {func: '次要功能2', allow: true, active: false}
+        {func: '次要功能1', route: '/dd', allow: true, active: false },
+        {func: '次要功能2', route: '/ee', allow: true, active: false}
     ]
   },
   { main: '次要',
     state: 'open',
     funcs: [
-        {func: '次要功能1', allow: true, active: false },
-        {func: '次要功能2', allow: true, active: false}
+        {func: '次要功能1', route: '/ff', allow: true, active: false },
+        {func: '次要功能2', route: '/gg', allow: true, active: false}
     ]
   }
 ];
